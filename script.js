5f01f469d748d1e25bcbe85d381b07f6ba7aa4a3
Intfly <71024388+Intfly@users.noreply.github.com>
2022-02-22 20:05:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,60 +12,58 @@ function menu(){
     }
 }
 
-window.onload=function(){
-    const base = document.querySelectorAll('.d1');
-    const box = document.querySelectorAll('.compopc');
-    
-    
-    
-    base.addEventListener('dragstart', dragStart);
-    base.addEventListener('dragend', dragEnd);
-    
-    
-    function dragStart() {
-        this.className += ' tenu';
-    
-        setTimeout(() => (this.className = 'invisible'), 0);
-    }
-    
-    function dragEnd() {
-        this.className = 'base';
-    }
-    
-    
-    for (const vide of box) {
-    
-        vide.addEventListener('dragover', dragOver);
-    
-        vide.addEventListener('dragenter', dragEnter);
-    
-        vide.addEventListener('dragleave', dragLeave);
-    
-        vide.addEventListener('drop', dragDrop);
-    
-    
-    }
-    
-    
-    
-    function dragOver(e) {
-        e.preventDefault()
-    
-    
-    }
-    
-    function dragEnter(e) {
-        e.preventDefault();
-        this.className += ' hovered';
-    }
-    
-    function dragLeave() {
-        this.className = 'compopc';
-    }
-    
-    
-    function dragDrop() {
-        this.className = 'compopc';
-        this.append(base);
-    }
-  }
+const base = document.querySelectorAll('.d1');
+const box = document.querySelectorAll('.compopc');
+
+
+
+base.addEventListener('dragstart', dragStart);
+base.addEventListener('dragend', dragEnd);
+
+
+function dragStart() {
+    this.className += ' tenu';
+
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'base';
+}
+
+
+for (const vide of box) {
+
+    vide.addEventListener('dragover', dragOver);
+
+    vide.addEventListener('dragenter', dragEnter);
+
+    vide.addEventListener('dragleave', dragLeave);
+
+    vide.addEventListener('drop', dragDrop);
+
+
+}
+
+
+
+function dragOver(e) {
+    e.preventDefault()
+
+
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'compopc';
+}
+
+
+function dragDrop() {
+    this.className = 'compopc';
+    this.append(base);
+}
